1dc2ea719660d40a9f64bca894ba62106b7dd81e
Pratham Mittal <mittalpra101@gmail.com>
2023-11-02 10:32:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+const accessKey = "bEvTk47j2b_DAGQx9WjW7VKm1nOZisYlagrPtegl-Zg";
+
+
+const searchForm=document.getElementById('search-form')
+const searchBox=document.getElementById('search-box')
+const searchResult=document.getElementById('search-result')
+const showMoreBtn=document.getElementById('show-more-btn')
+
+
+let keyword="";
+let page=1;
+
+async function searchImages(){
+    keyword=searchBox.value;
+    const url=`https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
+
+    const response= await fetch(url);
+    const data= await response.json();
+
+    if(page ==1){
+        searchResult.innerHTML="";
+    }
+
+    // console.log(data)
+
+    const results=data.results;
+
+    results.map((result)=>{ //here result is itself an arrow function passed as an argument in map function
+        const image=document.createElement("img"); //creates an img tag in html
+        image.src=result.urls.small; //locating the image in the api (inside "small" block within the "urls" block )
+        const imageLink=document.createElement("a"); //creates an anchor tag in html
+        imageLink.href=result.links.html;
+        imageLink.target="_blank";
+
+        imageLink.appendChild(image);
+        searchResult.appendChild(imageLink);
+    })
+    showMoreBtn.style.display="block";
+
+}
+
+searchForm.addEventListener("submit",(e) =>{
+    e.preventDefault();
+    page=1;
+    searchImages();
+})
+
+showMoreBtn.addEventListener("click",()=>{
+    page++;
+    searchImages();
+})
+
